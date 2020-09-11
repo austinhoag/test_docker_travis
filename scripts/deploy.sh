f1c4c36fcf278b070c6a8aa84ca9d010bf62534b
Austin Hoag <austinthomashoag@gmail.com>
2020-09-11 16:01:48 -0400

    codecov was not connecting; try simpler deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 id_rsa # Allow read access to the private key
-ssh-add id_rsa # Add the private key to SSH
+# eval "$(ssh-agent -s)" # Start ssh-agent cache
+# chmod 600 id_rsa # Allow read access to the private key
+# ssh-add id_rsa # Add the private key to SSH
 
-ssh ahoag@192.168.1.171 <<EOF
-  cd /home/ahoag/Git/test_docker_travis
-  ./start_server.sh
-EOF
+# ssh ahoag@192.168.1.171 <<EOF
+#   cd /home/ahoag/Git/test_docker_travis
+#   ./start_server.sh
+# EOF
+echo "success!"
